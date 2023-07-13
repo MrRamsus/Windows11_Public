80d357b82f3f375b60d79bd911ae66e2b960a0ef
MrRamsus <104757105+MrRamsus@users.noreply.github.com>
2023-07-13 15:21:06 +0200

    Update CopyChromiumProfiles.ps1
    Layout changes

--- CopyChromiumProfiles.ps1
+++ CopyChromiumProfiles.ps1
@@ -3,31 +3,31 @@
 	Copy template settings Chromium (Brave / Edge) profiles to all other profiles.
 .DESCRIPTION
 	With this script, the following actions will be done:
-    - Stop the browser process
-    - Profile folders will be cleaned
-    - Profile folders will be filled with the template profile files
+		- Stop the browser process
+		- Profile folders will be cleaned
+		- Profile folders will be filled with the template profile files
 .PARAMETER message
-    $Browser = Choose the browser you use. [brave | chrome | msedge]
-    $UserName = This is your windows username as shown in "c:\users"
-    $TemplateFolderName = The folder name of your default profile (Template profile) [Line under #Vars]
-    $ProfilePrefix = The prefixname of all profiles [Line under #Vars]
-    $ExcludeFolderFile = A filename which is be used to exclude that folder profile for this actions [Line under #Vars]
-    $UserData = The location of the browser user data. This value will automatically be filled after you choose your $Browser
-      - For brave use: "C:\Users\$UserName\AppData\Local\BraveSoftware\Brave-Browser\User Data"
-      - For chrome use: "C:\Users\$UserName\AppData\Local\Google\Chrome\User Data"
-      - For edge use: "C:\Users\$UserName\AppData\Local\Microsoft\Edge\User Data"
-    $ProcessName = The process name of the Brave (chromium) application. This value will automatically be filled after you choose your $Browser
-      - For Brave use: "brave"
-      - For chrome use: "chrome"
-      - For Edge use: "msedge"
+	$Browser = Choose the browser you use. [brave | chrome | msedge]
+	$UserName = This is your windows username as shown in "c:\users"
+	$TemplateFolderName = The folder name of your default profile (Template profile) [Line under #Vars]
+	$ProfilePrefix = The prefixname of all profiles [Line under #Vars]
+	$ExcludeFolderFile = A filename which is be used to exclude that folder profile for this actions [Line under #Vars]
+	$UserData = The location of the browser user data. This value will automatically be filled after you choose your $Browser
+		- For brave use: "C:\Users\$UserName\AppData\Local\BraveSoftware\Brave-Browser\User Data"
+		- For chrome use: "C:\Users\$UserName\AppData\Local\Google\Chrome\User Data"
+		- For edge use: "C:\Users\$UserName\AppData\Local\Microsoft\Edge\User Data"
+	$ProcessName = The process name of the Brave (chromium) application. This value will automatically be filled after you choose your $Browser
+		- For Brave use: "brave"
+		- For chrome use: "chrome"
+		- For Edge use: "msedge"
 .EXAMPLE
-How to use this script:
-	1. When you start the browser for the first time, you need to create one profile as template. Change every setting you need to use for other profiles.
- 	2. You have to know which browser you use. Change the var "$Browser" to the correct browser.
-    	3. Change the value "$UserName" with your windows username shown in "c:\users"
- 	4. Notice your template profile folder name (For Brave and Chrome, the first profile will be named "Default". In Edge, the first profile will be named "Profile 1". Check it in the app data and check which profile has been changed.
-   	5. If needed, create a file to exclude that profile for this script. In that case, the template settings will not be copied to that profile.
-    	6. Run the script.
+	How to use this script:
+		1. When you start the browser for the first time, you need to create one profile as template. Change every setting you need to use for other profiles.
+	 	2. You have to know which browser you use. Change the var "$Browser" to the correct browser.
+	    	3. Change the value "$UserName" with your windows username shown in "c:\users"
+	 	4. Notice your template profile folder name (For Brave and Chrome, the first profile will be named "Default". In Edge, the first profile will be named "Profile 1". Check it in the app data and check which profile has been changed.
+	   	5. If needed, create a file to exclude that profile for this script. In that case, the template settings will not be copied to that profile.
+	    	6. Run the script.
 .EXAMPLE
 	PS> ./CopyChromiumProfiles.ps1
 .LINK
